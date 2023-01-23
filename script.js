0045af7360a83da2318120b7146d51a531e6435c
matleg <mathieu.leger05@gmail.com>
2023-01-23 13:35:50 +0100

    create index, style and script files

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a1, a2) {
+    let add = a1 + a2;
+    return add;
+}
+
+function subtract(s1, s2) {
+     let subtract = s1 - s2;
+     return subtract;
+}
+
+function multiply(m1, m2) {
+    let multiply = m1 * m2;
+    return multiply;
+}
+
+function divide(d1, d2) {
+    let divide = d1 / d2;
+    return divide;
+}
+
+function operate() {
+    
+}
